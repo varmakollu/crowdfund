bccfb89c0464f8e2c5d371dd38e1e86a5a382454
Varma Kollu <varmaprince24@yahoo.com>
2025-02-23 16:32:02 +0530

    CrowdFund

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import './App.css';
+
+export default function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-50">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
